12b7a95a6f8bb5df76243f4792269a5eceba7877
AlbertoPuggioni <puggioni.alberto@gmail.com>
2023-11-30 21:49:43 +0100

    Implementato dell'aggiornamento dei dati dalle API

--- main.py
+++ main.py
@@ -1,33 +1,59 @@
-# Importazione delle librerie necessarie
 import requests
 import mysql.connector
 
-# Effettua una richiesta GET all'API designata
-response = requests.get('https://jsonplaceholder.typicode.com/posts/17')
+def get_api_data(api_url):
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
 
-# Check per vedere se la richiesta ha avuto successo (restituisce il codice di risposta HTTP 200)
-if response.status_code == 200:
-    # Estrae i dati JSON dalla risposta
-    data = response.json()
+def fetch_data_from_database(cursor, post_id):
+    cursor.execute('SELECT postId, userId, title, body FROM posts WHERE postId = %s', (post_id,))
+    return cursor.fetchone()
+
+def update_or_insert_data_in_database(data, cursor):
+    try:
+        cursor.execute('INSERT INTO posts (postId, userId, title, body) VALUES (%s, %s, %s, %s) '
+                       'ON DUPLICATE KEY UPDATE userId = VALUES(userId), title = VALUES(title), body = VALUES(body)',
+                       (data['id'], data['userId'], data['title'], data['body']))
+        print("Dati inseriti o aggiornati nel database con successo.")
+    except mysql.connector.Error as err:
+        print(f"Errore durante l'operazione nel database: {err}")
+        raise
+
+def main():
+    api_url = 'https://jsonplaceholder.typicode.com/posts'
+    db_config = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': '',
+        'database': 'prova'
+    }
 
     # Connessione al database MySQL
-    conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='prova'
-    )
-    cursor = conn.cursor()
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
 
-    # Inserimento dei dati nel database
-    cursor.execute('INSERT INTO posts (postId, userId, title, body) VALUES (%s, %s, %s, %s)',
-                   (data['id'], data['userId'], data['title'], data['body']))
+        # Ottieni dati dall'API
+        api_data_list = get_api_data(api_url)
 
-    # Conferma e chiude la connessione con il db
-    conn.commit()
-    conn.close()
+        for api_data in api_data_list:
+            # Inserisci o aggiorna dati nel database
+            update_or_insert_data_in_database(api_data, cursor)
 
-# Check per vedere se i dati sono stati inseriti con successo nel db, in caso contrario restituisce un errore
-    print("Dati inseriti nel database con successo.")
-else:
-    print(f"Errore nella richiesta API: {response.status_code}")
+        # Conferma e chiude la connessione con il database
+        conn.commit()
+
+    except Exception as e:
+        print(f"Errore durante l'esecuzione dello script: {e}")
+
+    finally:
+        # Chiude la connessione con il database
+        cursor.close()
+        conn.close()
+
+if __name__ == "__main__":
+    main()
