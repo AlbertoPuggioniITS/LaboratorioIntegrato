8003178a347579ee0561289583528725d7e8c64b
AlbertoPuggioni <puggioni.alberto@gmail.com>
2023-12-19 08:52:03 +0100

    correzioni

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def run_jobs():
 # Schedule dell'esecuzione dello script ogni sabato alle ore 01:00
 def main():
     # Schedule dell'esecuzione dello script ogni sabato alle ore 01:00
-    schedule.every().saturday.at("00:05").do(run_jobs)
+    schedule.every().saturday.at("01:00").do(run_jobs)
 
     # Loop per eseguire il programma
     while True:
